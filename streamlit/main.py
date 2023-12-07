dd6f89191c8115dfe40fd3b4d5b2fc688ae995d9
Felix Link <felix.link@maibornwolff.de>
2023-12-07 12:09:58 +0100

    feat: charts and temp convert slider

--- streamlit/main.py
+++ streamlit/main.py
@@ -3,7 +3,7 @@ import pandas
 
 data = {
     'Series_1': [1, 2, 3, 4, 5],
-    'Series_2': [10, 20, 30, 40, 50]
+    'Series_2': [9, 23, 25, 40, 129]
 }
 
 df = pandas.DataFrame(data)
@@ -15,3 +15,8 @@ Enjoy it!
 ''')
 
 st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celsius')
+st.write(myslider, "in Fahrenheit is ", myslider * 9/5 + 32)
