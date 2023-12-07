7ac9ccd6470d9c28e77d3e157c85c67b35591e1c
Felix Link <felix.link@maibornwolff.de>
2023-12-07 11:53:33 +0100

    inti

---
+++ streamlit/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Indroducing Streamlit in Automate Everything with Python')
+st.write('''This is our Web App.
+Enjoy it!
+''')
